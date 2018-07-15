cfbf4d7ea5bb42f8b661f6631b20da46ac8a67a6
Justin Wang <justinw827@aol.com>
2018-07-15 23:09:12 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(numIn)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(numIn)
+  count = numIn
+  while count > 0 do
+    puts "#{count} SECOND(S)!"
+    count -= 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
